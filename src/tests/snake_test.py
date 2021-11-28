505647e16bd26518ace1a9701c5e005009f784cb
Tatu Kuussaari <tatu@kuussaari.fi>
2021-11-28 19:46:41 +0200

    test added

--- src/tests/snake_test.py
+++ src/tests/snake_test.py
@@ -16,3 +16,13 @@ class TestSnake(unittest.TestCase):
 		y = snake.body[0].centery
 		self.assertEqual([x, y], [315, 285])
 
+	def test_change_direction(self):
+		snake = Snake()
+
+		snake.change_direction("left")
+		snake.move_snake()
+		x = snake.body[0].centerx
+		y = snake.body[0].centery
+		self.assertEqual([x, y], [285, 315])
+
+
