cd079308e5f2cc7a195352166b54750e3ff2878b
Tatu Kuussaari <tatu@kuussaari.fi>
2021-11-28 19:39:11 +0200

    added direction controls

--- src/index.py
+++ src/index.py
@@ -1,11 +1,21 @@
 import pygame
 from snake import Snake
 
+def check_direction(key, dir):
+	if key == pygame.K_UP and dir == "down" or key == pygame.K_DOWN and dir == "up" or key == pygame.K_RIGHT and dir == "left" or key == pygame.K_LEFT and dir == "right":
+		return False
+	return True
+
+
 pygame.init()
+
+
+
 
 snake = Snake()
 screen = pygame.display.set_mode((630,630))
 clock = pygame.time.Clock()
+dir = "up"
 
 running = True
 while running:
@@ -15,6 +25,20 @@ while running:
 		if event.type == pygame.KEYDOWN:
 			if event.key == pygame.K_ESCAPE:
 				running = False
+			if check_direction(event.key, dir):
+				if event.key == pygame.K_UP:
+					snake.change_direction("up")
+					dir = "up"
+				elif event.key == pygame.K_DOWN:
+					snake.change_direction("down")
+					dir = "down"
+				elif event.key == pygame.K_LEFT:
+					snake.change_direction("left")
+					dir = "left"
+				elif event.key == pygame.K_RIGHT:
+					snake.change_direction("right")
+					dir = "right"
+				break
 	snake.move_snake()
 	screen.fill([0, 0, 0])
 	if snake.check_bounds():
--- src/snake.py
+++ src/snake.py
@@ -1,17 +1,16 @@
 import pygame
 
-direction = [0, -30]
-
 class Snake:
 	def __init__(self):
 		self.body = []
 		self.body.append(pygame.Rect(300, 300, 30, 30))
+		self.direction = [0, -30]
 
 	def draw_snake(self, screen):
 		pygame.draw.rect(screen, (255,0,0), self.body[0])
 
 	def move_snake(self):
-		self.body[0].move_ip(direction)
+		self.body[0].move_ip(self.direction)
 
 	def check_bounds(self):
 		x = self.body[0].centerx
@@ -19,3 +18,13 @@ class Snake:
 		if x > 630 or x < 0 or y > 630 or y < 0:
 			return True
 		return False
+
+	def change_direction(self, dir):
+		if dir == "up":
+			self.direction = [0, -30]
+		if dir == "down":
+			self.direction = [0, 30]
+		if dir == "left":
+			self.direction = [-30, 0]
+		if dir == "right":
+			self.direction = [30, 0]
