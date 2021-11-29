73398676e426682da7444b06fa4223a4a59f13d4
Tatu Kuussaari <tatu@kuussaari.fi>
2021-11-29 21:15:21 +0200

    pylint fixes

--- src/index.py
+++ src/index.py
@@ -8,35 +8,48 @@ def check_direction(key, way):
         return False
     return True
 
-pygame.init()
-snake = Snake()
-screen = pygame.display.set_mode((630,630))
-clock = pygame.time.Clock()
-running = True
+def set_direction(event, snake, way):
+    if event.key == pygame.K_UP:
+        snake.change_direction("up")
+        way = "up"
+    elif event.key == pygame.K_DOWN:
+        snake.change_direction("down")
+        way = "down"
+    elif event.key == pygame.K_LEFT:
+        snake.change_direction("left")
+        way = "left"
+    elif event.key == pygame.K_RIGHT:
+        snake.change_direction("right")
+        way = "right"
+    return way
 
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
-                running = False
-            if check_direction(event.key, way):
-                if event.key == pygame.K_UP:
-                    snake.change_wayection("up")
-                    way = "up"
-                elif event.key == pygame.K_DOWN:
-                    snake.change_wayection("down")
-                    way = "down"
-                elif event.key == pygame.K_LEFT:
-                    snake.change_wayection("left")
-                    way = "left"
-                elif event.key == pygame.K_RIGHT:
-                    snake.change_wayection("right")
-                    way = "right"
-                break
-    snake.move_snake()
-    screen.fill([0, 0, 0])
-    if snake.check_bounds():
-        break
-    snake.draw_snake(screen)
-    pygame.display.update()
-    clock.tick(3)
+def quit_game(event):
+    if event.key == pygame.K_ESCAPE:
+        return False
+    return True
+
+def game():
+    pygame.init()
+    snake = Snake()
+    screen = pygame.display.set_mode((630,630))
+    clock = pygame.time.Clock()
+    running = True
+    way = "up"
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                running = quit_game(event)
+                if check_direction(event, way):
+                    way = set_direction(event, snake, way)
+                    break
+        snake.move_snake()
+        screen.fill([0, 0, 0])
+        if snake.check_bounds():
+            break
+        snake.draw_snake(screen)
+        pygame.display.update()
+        clock.tick(3)
+
+if __name__ == "__main__":
+    game()
